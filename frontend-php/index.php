621159c9a94a237f3b19e81edf3c5cc9c558d0d3
Karl Svartholm <gorbiz@gmail.com>
2012-04-03 07:09:31 +0200

    Autofocus implemented. Also cleaned up the HTML.

--- frontend-php/index.php
+++ frontend-php/index.php
@@ -1,8 +1,17 @@
 <?php
 require_once 'thought-logger/credentials.php';
-?><html>
+?>
+<!DOCTYPE html>
+<html>
 <head>
+    <meta charset="utf-8" />
 	<title>Kalle's thought-logger</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#content").focus();
+        });
+    </script>
 </head>
 
 <body>
