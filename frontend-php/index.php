888458088212ed1b621fda4beaf57168fc033d91
Karl Svartholm <gorbiz@gmail.com>
2012-06-04 22:14:15 +0200

    Made it beautiful, all black, no borders, centered large input - sweet.

--- frontend-php/index.php
+++ frontend-php/index.php
@@ -9,28 +9,64 @@
     <link rel="apple-touch-icon-precomposed" href="thought-logger-icon.png" />
 
     <style type="text/css">
+        body {
+            position: relative;
+            height: 100%;
+            background: #000;
+        }
         input {
             width: 100%;
-            font-size: 2em;
+            margin-top: -20px; /* Compensate for permanent header */
+            font-size: 3em;
             border: 0;
+            text-align: center;
+            background: #000;
+            color: #bbb;
+            font-family: Helvetica, arial, sans-serif;
+        }
+
+        input:focus {
+            outline: none;
+        }
+        * {
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);	
         }
     </style>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript">
         $(document).ready(function() {
+            
+            jQuery.fn.vertical_center = function () {
+                this.css("position","absolute");
+                this.css("top",
+                        (($(window).height() - this.outerHeight()) / 2) + 
+                        $(window).scrollTop() + "px");
+                return this;
+            }            
+
             // Hack around autocomplete="off" issue on Android
             $("#content").attr("name", "field_" + (new Date().getTime()));
             
             function hideAddressBar(){
-                if (document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
-                    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
-                setTimeout(function() { window.scrollTo(1,1) } ,0);
+                if (document.documentElement.scrollHeight < window.outerHeight / window.devicePixelRatio)
+                    document.documentElement.style.height = (window.outerHeight / window.devicePixelRatio + 1) + 'px';
+                setTimeout(function() { window.scrollTo(1, 1) } ,0);
             }
             window.addEventListener("load",function(){hideAddressBar();});
             window.addEventListener("orientationchange",function(){hideAddressBar();});
 
+            // Vertical alignment
+            $("#content").vertical_center();
+            $(window).bind('resize', function() {
+                $("#content").vertical_center();
+            });
+            
+            // Keep the focus on the text field
             $("#content").focus();
+            $("html").live('click', function() {
+                $("#content").focus();
+            });
 
             $("#form").submit(function() {
                 var data = {content: $("#content").val()};
