a597b99b1bf6a8d3aeb4ead896bf30d97825a1c4
Karl Svartholm <gorbiz@gmail.com>
2012-06-04 20:22:56 +0200

    Added a neat smart phone icon and hid the address bar on Androids on load.

--- frontend-php/index.php
+++ frontend-php/index.php
@@ -1,14 +1,20 @@
-<?php
-require_once 'thought-logger/credentials.php';
-?>
-<!DOCTYPE html>
+<?php require_once 'thought-logger/credentials.php'; ?><!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
-	<title>Kalle's thought-logger</title>
+    <title>Kalle's thought-logger</title>
+    <link rel="apple-touch-icon" href="thought-logger-icon.png" />
+    <link rel="apple-touch-icon-precomposed" href="thought-logger-icon.png" />
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript">
         $(document).ready(function() {
+
+            // "Hide" the browser bar on load
+            if(navigator.userAgent.match(/Android/i)){
+                window.scrollTo(0,1);
+            }
+
             $("#content").focus();
 
             $("#form").submit(function() {
