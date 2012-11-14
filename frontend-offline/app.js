d50943902c6e2248cee13c249a3ea4aedc6bd756
Karl Svartholm <gorbiz@gmail.com>
2012-11-14 10:11:37 +0100

    Refactored the server side code ensuring that files have the correct MIME Type headers.

--- frontend-offline/app.js
+++ frontend-offline/app.js
@@ -1,31 +1,4 @@
 var express = require("express");
-var fs = require("fs");
 var app = express();
 
-//app.use(express.errorHandler());
-app.use(express.logger('dev'));
-
-app.get("/cache.manifest", function(req, res){
-  fs.readFile('cache.manifest', 'utf8', function (err, data) {
-    res.set("Content-Type", "text/cache-manifest");
-    res.send(data.toString());
-  });
-});
-
-app.get("/index.html", function(req, res){
-  fs.readFile('index.html', 'utf8', function (err, data) {
-    res.set('Content-Type', 'text/html');
-    res.send(data.toString());
-  });
-});
-
-app.get("/jquery.min.js", function(req, res){
-  fs.readFile('jquery.min.js', 'utf8', function (err, data) {
-    res.set("Content-Type", "text/javascript");
-    res.send(data.toString());
-  });
-});
-
-
-app.listen(8080);
-console.log('listening on 8080');
+app.use(express.static(__dirname)).listen(8080);
